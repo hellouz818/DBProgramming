37439a33dc62165ae926c27aa92e7fc5f5d7aaa2
young <jinyoung7165@gmail.com>
2021-06-03 00:39:23 +0900

    Update InsertEnroll_test.sql

--- InsertEnroll_test.sql
+++ InsertEnroll_test.sql
@@ -1,16 +1,9 @@
-DECLARE
-result VARCHAR2(50) := '';
-
-BEGIN
-DBMS_OUTPUT.enable;
-DBMS_OUTPUT.PUT_LINE('******테스트*******');
-InsertEnroll('1400002', 'C100', 1, result);
-DBMS_OUTPUT.PUT_LINE('결과' || result);
-InsertEnroll('1400002', 'C100', 2, result);
-DBMS_OUTPUT.PUT_LINE('결과' || result);
-DBMS_OUTPUT.PUT_LINE('CURSOR를 이용한 테스트');
-selectTimeTable('1400002', 2021, 2);
-DELETE FROM enroll WHERE s_id = '140002' and c_no = 'C100' and split_no =1;
-
-END;
+declare
+  result VARCHAR2(50) := '';
+ begin
+ dbms_output.enable;
+ dbms_output.put_line('******테스트*******');
+ InsertEnroll('1916205', 'C405', 1, result);
+dbms_output.put_line('결과' || result);
+end;
 /
