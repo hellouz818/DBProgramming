2cff4946add5b45c4053d3af192c84fbdcadf8fe
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2021-06-03 02:51:11 +0900

    Update InsertEnroll_test.sql

--- InsertEnroll_test.sql
+++ InsertEnroll_test.sql
@@ -3,7 +3,7 @@ declare
  begin
  dbms_output.enable;
  dbms_output.put_line('******테스트*******');
- InsertEnroll('1916205', 'C405', 1, result);
+ InsertEnroll('1900002', 'A302', 1,'내가만드는3D프린팅' ,result);
 dbms_output.put_line('결과' || result);
 end;
 /
