d29f12e90f222f354b7cf479bf43dcefbff87945
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2021-06-02 21:28:01 +0900

    Update teach.sql

--- Table/teach.sql
+++ Table/teach.sql
@@ -9,7 +9,7 @@ CREATE TABLE teach
     t_professor    VARCHAR2(30)    NOT NULL, 
     place          VARCHAR2(30)    NOT NULL, 
     c_no           VARCHAR2(30)    NOT NULL, 
-    c_name         VARCHAR2(30)    NOT NULL, 
+    c_name         VARCHAR2(60)    NOT NULL, 
     c_grade        NUMBER    NOT NULL, 
     CONSTRAINT teach_pk PRIMARY KEY (t_year, t_semester, split_no, c_no)
 );
