4ec81f7afa030557f08ed9c035cd08bf99b35d15
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2021-06-03 16:23:48 +0900

    Update InsertEnroll_test.sql

--- InsertEnroll_test.sql
+++ InsertEnroll_test.sql
@@ -1,9 +1,16 @@
 declare
-  result VARCHAR2(50) := '';
- begin
- dbms_output.enable;
- dbms_output.put_line('******테스트*******');
- InsertEnroll('1900002', 'A302', 1,'내가만드는3D프린팅' ,result);
-dbms_output.put_line('결과' || result);
+    result VARCHAR2(100) := '';
+begin
+    dbms_output.enable;
+    dbms_output.put_line('******테스트*****');
+    InsertEnroll('1900003', 'A302', 1, result);
+    InsertEnroll('1900003', 'C304', 2, result);
+    InsertEnroll('1900003', 'C307', 3, result);
+    InsertEnroll('1900003', 'C307', 1, result);
+    InsertEnroll('1900003', 'L202', 1, result);
+    InsertEnroll('1900003', 'L302', 1, result);
+    dbms_output.put_line('결과' || result);
+    dbms_output.put_line('시간표 출력');
+    SelectTimeTable('1900002', 2021, 2);
 end;
 /
