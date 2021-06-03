2488603d36b3ebb49b4b6300208fc60db5c400b8
Su-hwa Yu <65851554+SpeakingPotato@users.noreply.github.com>
2021-06-03 15:45:46 +0900

    Update InsertEnroll.sql

--- InsertEnroll.sql
+++ InsertEnroll.sql
@@ -1,22 +1,32 @@
 CREATE OR REPLACE PROCEDURE InsertEnroll(sStudentId IN VARCHAR2,
   sCourseId IN VARCHAR2,
-  nCourseIdNo IN NUMBER,
-  result  OUT VARCHAR2)
+  nCourseIdNo IN NUMBER, result  OUT VARCHAR2)
   IS
     too_many_sumCourseUnit  EXCEPTION;
     too_many_courses  EXCEPTION;
     too_many_students  EXCEPTION;
     duplicate_time  EXCEPTION;
+
     nYear  NUMBER;
     nSemester  NUMBER;
+
+    nCname course.c_name%type;
     nSumCourseUnit  NUMBER;
     nCourseUnit  NUMBER;
     nCnt  NUMBER;
     nTeachMax  NUMBER;
-nCname VARCHAR2(60);
+
   BEGIN
     result := '';
-    nTeachMax:=3;
+
+select c_name
+into nCname
+from course
+where c_no=sCourseId;
+
+
+
+nTeachMax:=0;
   DBMS_OUTPUT.put_line('#');
   DBMS_OUTPUT.put_line(sStudentId || '님이 과목번호 ' || sCourseId ||
   ', 분반 ' || TO_CHAR(nCourseIdNo) || '의 수강 등록을 요청하였습니다.');
@@ -29,17 +39,18 @@ nCname VARCHAR2(60);
     INTO nSumCourseUnit
     FROM   teach t, enroll e
     WHERE  e.s_id = sStudentId and e.year = nYear and
-         e.semester = nSemester  and  e.c_name = t.c_name and e.c_no = t.c_no;
+         e.semester = nSemester  and  e.c_name = t.c_name and e.c_no = t.c_no and e.split_no=t.split_no;
 
-    SELECT c_grade,c_name
-    INTO nCourseUnit,nCname
+    SELECT c_grade
+    INTO nCourseUnit
     FROM teach
-    WHERE c_no = sCourseId and split_no=nCourseIdNo;
+    WHERE c_no = sCourseId and split_no=nCourseIdNo and t_year=nYear and t_semester=nSemester;
 
     IF (nSumCourseUnit + nCourseUnit > 18)
     THEN
        RAISE too_many_sumCourseUnit;
     END IF;
+
   /* 에러 처리 2 : 동일한 과목 신청 여부 */
     SELECT COUNT(*)
     INTO nCnt
@@ -52,17 +63,19 @@ nCname VARCHAR2(60);
     END IF;
 
     /* 에러 처리 3 : 수강신청 인원 초과 여부 */
-   /*SELECT t_max
-    INTO nTeachMax
+    SELECT t_max
+    INTO nTeachMax  
     FROM   teach
     WHERE  t_year= nYear and t_semester = nSemester
-   and c_no = sCourseId and split_no= nCourseIdNo; */
+   and c_no = sCourseId and split_no= nCourseIdNo;
 
     SELECT COUNT(*)
     INTO   nCnt
     FROM   enroll
     WHERE  year = nYear and semester = nSemester
            and c_no = sCourseId and split_no = nCourseIdNo;
+
+
     IF (nCnt >= nTeachMax)
     THEN
        RAISE too_many_students;
@@ -82,7 +95,7 @@ nCname VARCHAR2(60);
     FROM  teach t, enroll e
     WHERE  e.s_id=sStudentId and e.year=nYear and e.semester = nSemester and
   t.t_year=nYear and t.t_semester = nSemester and
-  e.c_name=t.c_name and e.c_no=t.c_no
+  e.c_name=t.c_name and e.c_no=t.c_no and e.split_no=t.split_no
     );
     IF (nCnt > 0)
     THEN
@@ -90,10 +103,47 @@ nCname VARCHAR2(60);
     END IF;
 
  /* 수강 신청 등록 */
-    INSERT INTO enroll(S_ID,YEAR,SEMESTER,C_NO,C_NAME,SPLIT_NO)
-    VALUES (sStudentId, nYear, nSemester, sCourseId, nCname,nCourseIdNo);
+    INSERT INTO enroll(S_ID,YEAR,SEMESTER, C_NO,C_NAME,SPLIT_NO)
+    VALUES (sStudentId, nYear,nSemester, sCourseId, nCname,nCourseIdNo);
   COMMIT;
     result := '수강신청 등록이 완료되었습니다.';
-  
+  EXCEPTION
+    WHEN too_many_sumCourseUnit  THEN
+   result := '최대학점을 초과하였습니다';
+    WHEN too_many_courses   THEN
+   result := '이미 등록된 과목을 신청하였습니다';
+    WHEN too_many_students  THEN
+   result := '수강신청 인원이 초과되어 등록이 불가능합니다';
+    WHEN duplicate_time  THEN
+   result := '이미 등록된 과목 중 중복되는 시간이 존재합니다';
+    WHEN OTHERS THEN
+   ROLLBACK;
+         result :=nTeachMax;
+COMMIT;
 END;
 /
+
+
+declare
+  result VARCHAR2(100) := '';
+ begin
+ dbms_output.enable;
+ dbms_output.put_line('******테스트*******');
+ InsertEnroll('1900002', 'L202', 1 ,result);
+dbms_output.put_line('결과' || result);
+dbms_output.put_line('CURSOR를 이용할 테스트');
+SelectTimeTable('1900002', 2021, 2);
+end;
+/ 
+
+declare
+  result VARCHAR2(100) := '';
+ begin
+ dbms_output.enable;
+ dbms_output.put_line('******테스트*******');
+ InsertEnroll('1900002', 'L302', 1 ,result);
+dbms_output.put_line('결과' || result);
+dbms_output.put_line('CURSOR를 이용할 테스트');
+SelectTimeTable('1900002', 2021, 2);
+end;
+/ 
